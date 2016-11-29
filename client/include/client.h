d669652d91da0f81c3e999c59579007e3d724692
Ceres Carvalho | Stone <ccarvalho@stone.com.br>
2016-11-29 16:23:30 -0200

    :tada: Initial commit. Project structure.

---
+++ client/include/client.h
@@ -0,0 +1,7 @@
+#ifndef CLIENT
+#define CLIENT
+
+// Declaração de estruturas de dados, constantes, assinatura de métodos
+// públicos e macros.
+
+#endif _CLIENT_
---
+++ server/include/server.h
@@ -0,0 +1,7 @@
+#ifndef SERVER
+#define SERVER
+
+// Declaração de estruturas de dados, constantes, assinatura de métodos
+// públicos e macros.
+
+#endif _SERVER_
